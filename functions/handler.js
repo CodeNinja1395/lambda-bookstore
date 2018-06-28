5c885d6dd83b9c9f02b7a0eee8d51635ab57e7ce
Gregory Kysenko <heyapple1395@gmail.com>
2018-06-28 18:41:14 +0300

    edit && delete

--- functions/handler.js
+++ functions/handler.js
@@ -1,7 +1,9 @@
 'use strict';
 const AWS = require('aws-sdk');
 const uuid = require('uuid/v4');
-const {validateReq, validateOpt} = require('../model/validate');
+//const _ = require('underscore');
+const { validateReq, validateOpt } = require('../model/validate');
+const config = require('../config/config');
 
 const dbClient = new AWS.DynamoDB.DocumentClient({region: 'us-east-1'});
 
@@ -9,36 +11,88 @@ module.exports.homepage = (event, context, callback) => {
     const response = {
         statusCode: 200,
         body: JSON.stringify({
-             message: 'Go Serverless v1.0! Your function executed successfully!'   
+            message: config.homeMessage   
         })
     };
 
-  callback(null, response);
+    callback(null, response);
+};
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
+module.exports.getBook = (event, context, callback) => {
+    const params = {
+        TableName: process.env.DYNAMODB_TABLE,
+        Key: {
+            id: event.pathParameters.id
+        }
+    };
+
+    dbClient.get(params, (err, data) => {    
+        if (err) {
+            const response = {
+                statusCode: 404,
+                body: JSON.stringify(err)
+            };
+            callback(null, response);
+        }
+
+        if(!data.Item || data.Item.isDeleted) {
+            const response = {
+                statusCode: 404,
+                headers: { 
+                    'Content-Type': 'text/plain',
+                    'Access-Control-Allow-Origin': '*'
+                },
+                body: JSON.stringify('can`t find book with this id')
+            };
+
+            callback(null, response);
+        } 
+        
+        const response = {
+            statusCode: 200,
+            body: JSON.stringify(data.Item)
+        };
+
+        callback(null, response);
+    });
 };
 
 module.exports.getBooks = (event, context, callback) => {
     const params = {
-        TableName : 'books',
-        Item: {
-            id: 15
-        }
+        TableName: process.env.DYNAMODB_TABLE,
+        FilterExpression: 'isDeleted = :is_deleted',
+        ExpressionAttributeValues: {':is_deleted' : false}
     };
-    const result = dbClient.scan(params, function(err, data) {
-        if (err){
-            callback(err, null);
-        } else {
-            callback(null, data);
+    dbClient.scan(params, (err, data) => {
+        if (err) {
+            const response = {
+                statusCode: err.statusCode,
+                headers: { 
+                    'Content-Type': 'application/json',
+                    'Access-Control-Allow-Origin': '*'
+                },
+                body: JSON.stringify(err)
+            };
+            
+            callback(null, response);
         }
+        if(!data.Items) {
+            const response = {
+                statusCode: 200,
+                body: JSON.stringify('Table is empty')
+            }; 
+
+            callback(null, response);
+        }
+
+        const response = {
+            statusCode: 200,
+            body: JSON.stringify(data.Items)
+        };   
+
+        callback(null, response);
     });
-
-}
-
-module.exports.getBook = (event, context, callback) => {
-  
-}
+};
 
 module.exports.addBook = (event, context, callback) => {
     const body = JSON.parse(event.body);
@@ -46,33 +100,142 @@ module.exports.addBook = (event, context, callback) => {
         id: uuid(),
         title: body.title,
         author: body.author,      
-        added: Date.now(),
+        added: new Date(Date.now()).toString(),
         isDeleted: false
-    }    
-    try {                
+    };  
+    validateReq(book, (err, valid) => {
+        if(err) {
+            const response = {
+                statusCode: err.statusCode,
+                body: JSON.stringify(err)
+            };
+
+            callback(null, response); 
+        }
         const params = {
-            Item: book,
-            TableName: 'lambda-books-dev-BooksDynamoDbTable-18XSWIRSGOEA0'     
-        }  
- 
-        dbClient.put(params, function(err, data) {
+            Item: valid,
+            TableName: process.env.DYNAMODB_TABLE    
+        };
+
+        dbClient.put(params, (err, data) => {
             if (err) {
-                console.log(err);      
-                callback(err, null);
-            } else {
-                console.log(data);           
-                callback(null, data);
-            }
-            });    
-    } catch (err) {
-        callback(err, null);
-        console.log(err);       
-    }
-}
+                const response = {
+                    statusCode: err.statusCode,
+                    body: JSON.stringify(err)
+                };
+                callback(null, response);
+            } 
+            const response = {
+                statusCode: 200,
+                body: JSON.stringify(data)
+            };
+
+            callback(null, response);        
+        }); 
+    });  
+};
 
 module.exports.editBook = (event, context, callback) => {
+    const data = JSON.parse(event.body);
+    validateOpt(data, (err, valid) => {
+        if(err) {
+            const response = {
+                statusCode: 200,
+                body: JSON.stringify({
+                    error: err
+                })
+            };
 
-}
+            callback(null, response); 
+        }
+        const params = {
+            TableName: process.env.DYNAMODB_TABLE,
+            Key: {
+                id: event.pathParameters.id
+            },
+            AttributeUpdates: {}
+        };
+
+        Object.keys(valid).forEach((prop) => {
+            params.AttributeUpdates[prop] = {
+                Action: 'PUT',  
+                Value: valid[prop]
+            };       
+        });
+        
+        dbClient.update(params, (err, data) => {
+            if(err) {
+                const response = {
+                    statusCode: err.statusCode,
+                    body: JSON.stringify(err)
+                };
+
+                callback(null, response);
+            }
+            const response = {
+                statusCode: 200,
+                body: JSON.stringify(data.Item)
+            };
+
+            callback(null, response);
+        });     
+    });
+};
+
 module.exports.deleteBook = (event, context, callback) => {
+    const data = JSON.parse(event.body);  
+    console.log(data);
+    
+    const params = {
+        TableName : process.env.DYNAMODB_TABLE,
+        Key: {
+            id: event.pathParameters.id
+        },
+        AttributeUpdates: {}
+    };
 
-}
+    if (data.flush === config.flush && 
+        event.headers.Authorization === config.authorization) {
+        dbClient.delete(params, (err, data) => {
+            if (err) {
+                const response = {
+                    statusCode: err.statusCode,
+                    body: JSON.stringify(err)
+                };
+
+                callback(null, response);
+            }
+            const response = {
+                statusCode: 200,
+                body: JSON.stringify(data)
+            };
+
+            callback(null, response);
+        });
+    } else {
+        console.log(params);
+        
+        params.AttributeUpdates.isDeleted = {
+            Action: 'PUT',
+            Value: true
+        };
+        console.log(params);
+
+        dbClient.update(params, (err, data) => {
+            if (err) {
+                const response = {
+                    statusCode: err.statusCode,
+                    body: JSON.stringify(err)
+                };
+
+                callback(null, response);
+            }
+            const response = {
+                statusCode: 200,
+                body: JSON.stringify(data)
+            };
+
+            callback(null, response);
+        });   
+    }
+};
--- model/validate.js
+++ model/validate.js
@@ -4,24 +4,24 @@ const schemaRequired = Joi.object().keys({
     id: Joi.string().required(),
     title: Joi.string().regex(/^[a-zA-Z0-9\s]+$/).required(),
     author: Joi.string().regex(/^[a-zA-Z0-9\s]+$/).required(),
-    added: Joi.date(),
+    added: Joi.string(),
     isDeleted: Joi.boolean()
 });
 const schemaOptional = Joi.object().keys({
     id: Joi.string(),
     title: Joi.string().regex(/^[a-zA-Z0-9\s]+$/),
     author: Joi.string().regex(/^[a-zA-Z0-9\s]+$/),
-    added: Joi.date(),
+    added: Joi.string(),
     isDeleted: Joi.boolean()    
 });
 
-module.exports.validateReq = (object) => {
-   return Joi.validate(object, schemaRequired);
-}
+module.exports.validateReq = (object, callback) => {
+    return Joi.validate(object, schemaRequired, callback);
+};
 
 module.exports.validateOpt = (object, callback) => {
-    Joi.validate(object, schemaOptional);
-}
+    Joi.validate(object, schemaOptional, callback);
+};
 
 
 
