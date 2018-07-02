201c46ff290eda9dd205142fa1592fb0afa82845
Gregory Kysenko <heyapple1395@gmail.com>
2018-07-02 11:44:49 +0300

    promises added

--- functions/handler.js
+++ functions/handler.js
@@ -1,12 +1,9 @@
 'use strict';
-const AWS = require('aws-sdk');
 const uuid = require('uuid/v4');
-//const _ = require('underscore');
 const { validateReq, validateOpt } = require('../lib/validate');
 const config = require('../config/config');
 
-
-const dbClient = require('../lib/dynamodb');
+const docClient = require('../lib/dynamodb');
 
 module.exports.homepage = (event, context, callback) => {
     const response = {
@@ -27,36 +24,36 @@ module.exports.getBook = (event, context, callback) => {
         }
     };
 
-    dbClient.get(params, (err, data) => {    
-        if (err) {
+    docClient.get(params)
+        .then((data) => {
+            if(!data.Item || data.Item.isDeleted) {
+                const response = {
+                    statusCode: 404,
+                    headers: { 
+                        'Content-Type': 'text/plain',
+                        'Access-Control-Allow-Origin': '*'
+                    },
+                    body: JSON.stringify('can`t find book with this id')
+                };
+    
+                callback(null, response);
+            } 
+            
             const response = {
-                statusCode: 404,
+                statusCode: 200,
+                body: JSON.stringify(data.Item)
+            };
+    
+            callback(null, response);
+        })
+        .catch((err) => {
+            const response = {
+                statusCode: err.statusCode,
                 body: JSON.stringify(err)
             };
 
-            callback(null, response);
-        }
-
-        if(!data.Item || data.Item.isDeleted) {
-            const response = {
-                statusCode: 404,
-                headers: { 
-                    'Content-Type': 'text/plain',
-                    'Access-Control-Allow-Origin': '*'
-                },
-                body: JSON.stringify('can`t find book with this id')
-            };
-
-            callback(null, response);
-        } 
-        
-        const response = {
-            statusCode: 200,
-            body: JSON.stringify(data.Item)
-        };
-
-        callback(null, response);
-    });
+            callback(null, response); 
+        });
 };
 
 module.exports.getBooks = (event, context, callback) => {
@@ -66,36 +63,32 @@ module.exports.getBooks = (event, context, callback) => {
         ExpressionAttributeValues: {':is_deleted' : false}
     };
 
-    dbClient.scan(params, (err, data) => {
-        if (err) {
-            const response = {
-                statusCode: err.statusCode,
-                headers: { 
-                    'Content-Type': 'application/json',
-                    'Access-Control-Allow-Origin': '*'
-                },
-                body: JSON.stringify(err)
-            };
-            
-            callback(null, response);
-        }
-
-        if (!data.Items) {
+    docClient.scan(params)
+        .then((data) => {
+            if (!data.Items) {
+                const response = {
+                    statusCode: 200,
+                    body: JSON.stringify('Table is empty')
+                }; 
+    
+                callback(null, response);
+            }
+    
             const response = {
                 statusCode: 200,
-                body: JSON.stringify('Table is empty')
-            }; 
-
+                body: JSON.stringify(data.Items)
+            };   
+    
             callback(null, response);
-        }
+        })
+        .catch((err) => {
+            const response = {
+                statusCode: err.statusCode,
+                body: JSON.stringify(err)
+            };
 
-        const response = {
-            statusCode: 200,
-            body: JSON.stringify(data.Items)
-        };   
-
-        callback(null, response);
-    });
+            callback(null, response); 
+        });
 };
 
 module.exports.addBook = (event, context, callback) => {
@@ -109,15 +102,22 @@ module.exports.addBook = (event, context, callback) => {
     };  
 
     validateReq(book)
-        .then((valid) => {
-            console.log(valid);            
+        .then((valid) => {           
             return {
                 Item: valid,
                 TableName: process.env.DYNAMODB_TABLE    
             };
         })
-        .then((params) => {
-            dbClient.put
+        .then((params) => {      
+            return docClient.put(params);
+        })
+        .then((book) => {       
+            const response = {
+                statusCode: 200,
+                body: JSON.stringify(book)
+            };
+
+            callback(null, response); 
         })
         .catch((err) => {
             const response = {
@@ -127,90 +127,53 @@ module.exports.addBook = (event, context, callback) => {
 
             callback(null, response); 
         });
-
-    // validateReq(book, (err, valid) => {
-    //     if (err) {
-    //         const response = {
-    //             statusCode: err.statusCode,
-    //             body: JSON.stringify(err)
-    //         };
-
-    //         callback(null, response); 
-    //     }
-    //     const params = {
-    //         Item: valid,
-    //         TableName: process.env.DYNAMODB_TABLE    
-    //     };
-
-    //     dbClient.put(params, (err, data) => {
-    //         if (err) {
-    //             const response = {
-    //                 statusCode: err.statusCode,
-    //                 body: JSON.stringify(err)
-    //             };
-    //             callback(null, response);
-    //         } 
-    //         const response = {
-    //             statusCode: 200,
-    //             body: JSON.stringify(data)
-    //         };
-
-    //         callback(null, response);        
-    //     }); 
-    // });  
 };
 
 module.exports.editBook = (event, context, callback) => {
     const data = JSON.parse(event.body);
-    validateOpt(data, (err, valid) => {
-        if (err) {
+
+    validateOpt(data)
+        .then((valid) => {
+            const params = {
+                TableName: process.env.DYNAMODB_TABLE,
+                Key: {
+                    id: event.pathParameters.id
+                },
+                AttributeUpdates: {}
+            };
+            if (valid.title) {
+                params.AttributeUpdates.title = {
+                    Action: 'PUT',  
+                    Value: valid.title
+                };
+            }
+            if (valid.author) {
+                params.AttributeUpdates.author = {
+                    Action: 'PUT',  
+                    Value: valid.author
+                };
+            }
+            return params;
+        })
+        .then((params) => {
+            return docClient.update(params);
+        })
+        .then((book) => {
             const response = {
                 statusCode: 200,
-                body: JSON.stringify({
-                    error: err
-                })
+                body: JSON.stringify(book)
             };
 
             callback(null, response); 
-        }
-        const params = {
-            TableName: process.env.DYNAMODB_TABLE,
-            Key: {
-                id: event.pathParameters.id
-            },
-            AttributeUpdates: {}
-        };
-
-        if (valid.title) {
-            params.AttributeUpdates.title = {
-                Action: 'PUT',  
-                Value: valid.title
-            };
-        }
-        if (valid.author) {
-            params.AttributeUpdates.author = {
-                Action: 'PUT',  
-                Value: valid.author
-            };
-        }
-        
-        dbClient.update(params, (err, data) => {
-            if(err) {
-                const response = {
-                    statusCode: err.statusCode,
-                    body: JSON.stringify(err)
-                };
-
-                callback(null, response);
-            }
+        })
+        .catch((err) => {
             const response = {
-                statusCode: 200,
-                body: JSON.stringify(data.Item)
+                statusCode: err.statusCode,
+                body: JSON.stringify(err)
             };
 
-            callback(null, response);
-        });     
-    });
+            callback(null, response); 
+        });
 };
 
 module.exports.deleteBook = (event, context, callback) => {
@@ -225,43 +188,46 @@ module.exports.deleteBook = (event, context, callback) => {
 
     if (data.flush === config.flush && 
         event.headers.Authorization === config.authorization) {
-        dbClient.delete(params, (err, data) => {
-            if (err) {
+        docClient.remove(params)
+            .then((data) =>{
+                const response = {
+                    statusCode: 200,
+                    body: JSON.stringify(data)
+                };
+    
+                callback(null, response);
+            })
+            .catch((err) => {
                 const response = {
                     statusCode: err.statusCode,
                     body: JSON.stringify(err)
                 };
 
                 callback(null, response);
-            }
-            const response = {
-                statusCode: 200,
-                body: JSON.stringify(data)
-            };
-
-            callback(null, response);
-        });
+            });
     } else {
         params.AttributeUpdates.isDeleted = {
             Action: 'PUT',
             Value: true
         };
 
-        dbClient.update(params, (err, data) => {
-            if (err) {
+        docClient.update(params)
+            .then((data) => {
+                const response = {
+                    statusCode: 200,
+                    body: JSON.stringify(data)
+                };
+    
+                callback(null, response);
+            }) 
+            .catch((err) => {
                 const response = {
                     statusCode: err.statusCode,
                     body: JSON.stringify(err)
                 };
 
                 callback(null, response);
-            }
-            const response = {
-                statusCode: 200,
-                body: JSON.stringify(data)
-            };
-
-            callback(null, response);
-        });   
-    }
+            });
+    }   
 };
+
